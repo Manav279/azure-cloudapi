d42f9c58c79cb718752b77d369070d5a4879e57e
Manav279 <manavagarwal279@gmal.com>
2023-04-26 13:49:36 +0530

    README + Fixes

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# azure-cloudapi
+# azure-cloudapi v0.2.0
+
+## A fully functional API to access Azure Storage Account in Python with ease!
+
+This API is built to perform operations in an Azure Storage Account using python. You do not need to use the azure portal or need the connection string of storage account to access it.
+
+Operations Provided:
+- Upload Blobs to Container inside Storage.
+- Delete Blobs from Container inside Storage.
+- Download Blobs from Container inside Storage.
+- List all Blobs present inside the Container in Storage.
+
+# Features:
+- Access Storage Account using code with ease, without repeating unnecessary steps.
+- Generate Logs of all operations performed on the Storage with proper Date and Time of operation.
+- API built using FastAPI
+
+# How to use API
+To use the CloudAPI, follow these steps:
+
+1. Clone this project
+2. Install necessary libraries - 
+    - FastAPI - `pip install fastapi`
+    - Uvicorn - `pip install "uvicorn[standard]"`
+    - Azure Exceptions - `pip install azure-core`
+    - Azure Storage - `pip install azure-storage-blob`
+3. Add your Storage Account Connection String in main.py in variable `storage_conn_str` (Line 8)
+4. Add your Container Name in main.py in variable `container` (Line 10)
+5. In terminal change directory to where main.py is present, execute command `uvicorn main:app --reload`
+    Note: If error is thrown, try command `python -m uvicorn main:app --reload`
+6. API is running, use URL `http://127.0.0.1:8000` to interact with API
+
+# Changing Downloads Folder
+Files downloaded are by default stored in `Downloads/`
+Downloads directory can be changed by changing the `downloads_path` in main.py
+   Note: If you change the path, make sure following directory already exists to prevent errors 
+
+# Changing Logs File
+Operation Logs by default are stored in `CloudAPILogs.txt`
+Log file name can be changed by chaning the variable `log_file_name`
+You can also add log file into a directory by changeing name to `[directory_name]/[log_file_name].txt`
+    Note: Make sure directory already exists in system to prevent error
+
+# Command Examples
+
+1. Uploading Blob -
+    `http://127.0.0.1:8000/upload/C:/Users/Username/Downloads/data.csv`
+
+2. Delete Blob - 
+    `http://127.0.0.1:8000/delete/data.csv`
+
+3. Download Blob - 
+    `http://127.0.0.1:8000/download/data.csv`
+
+4. Generate List of Blobs -
+    `http://127.0.0.1:8000/list`
+
+# Future Additions
+
+- Config File
+- User Authentication
+- Blob Name increment for blobs with same names
+- FastAPI Documentation
+- Storing Logs in Cloud
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ container = "[Storage Container Name]"
 blob_service_client = BlobServiceClient.from_connection_string(storage_conn_str)
 
 # Function for adding logs of commands into file CloudAPILogs.txt (can change name)
-def addLog(cmmd, file_name):
+def addLog(cmmd, blob_name):
     log_file_name = "CloudAPILogs.txt" # Log File Name
     # You can also add log file into a directory by changeing name to "[directory_name]/[log_file_name].txt"
     # but make sure directory already exists in system to prevent error
@@ -20,13 +20,13 @@ def addLog(cmmd, file_name):
     logfile = open(log_file_name, "a")
     match cmmd:
         case "upload": 
-            logfile.write("{} => Uploaded file {}\n".format(currTime, file_name))
+            logfile.write("{} => Uploaded Blob {}\n".format(currTime, blob_name))
         case "delete": 
-            logfile.write("{} => Deleted file {}\n".format(currTime, file_name))
+            logfile.write("{} => Deleted Blob {}\n".format(currTime, blob_name))
         case "download": 
-            logfile.write("{} => Downloaded file {}\n".format(currTime, file_name))
+            logfile.write("{} => Downloaded Blob {}\n".format(currTime, blob_name))
         case "list": 
-            logfile.write("{} => Generated list of files\n".format(currTime))
+            logfile.write("{} => Generated list of Blobs\n".format(currTime))
     logfile.close()
 
 # Creating a FastAPI Application
@@ -41,49 +41,52 @@ async def welcome():
     msg = {"message": "Welcome to AzureCloudAPI!"}
     return msg
 
-#  ------ Upload File to Storage -----
+#  ------ Upload Blob to Storage -----
 @app.get("/upload/{loc:path}")
-async def upload_file(loc: str):
-    file_name = loc.split("/")[-1]
-    blob_client = blob_service_client.get_blob_client(container, blob=file_name)
+async def uploadBlob(loc: str):
+    blob_name = loc.split("/")[-1]
+    blob_client = blob_service_client.get_blob_client(container, blob=blob_name)
     with open(loc, "rb") as data:
         try:
                 blob_client.upload_blob(data)
-                status = "Uploaded {} successfully".format(file_name)
+                status = "Uploaded {} successfully".format(blob_name)
+                addLog("upload", blob_name)
         except ResourceExistsError:
-             status = "File already exists"
+             status = "Blob already exists"
     return {"status": status}
 
-#  ----- Delete File from Storage -----
+#  ----- Delete Blob from Storage -----
 @app.get("/delete/{blob}")
-async def delete_file(blob: str):
+async def deleteBlob(blob: str):
      container_client = blob_service_client.get_container_client(container)
      blobs = container_client.list_blobs()
      try:
             container_client.delete_blob(blob)
             status = "Successfully Deleted Blob {}".format(blob)
+            addLog("delete", blob)
      except ResourceNotFoundError:
-            status = "File does not exist"
+            status = "Blob does not exist"
      return status
 
 #  ----- Download a Blob -----
 @app.get("/download/{blob}")
-async def get_blob(blob: str):
+async def downloadBlob(blob: str):
     blob_client = blob_service_client.get_blob_client(container, blob=blob)
-    downloads_path = "Downloads/" # Path where files will be downloaded
+    downloads_path = "Downloads/" # Path where blobs will be downloaded
     # If you change the path, make sure following directory already exists to prevent errors
     try:
         blob_data = blob_client.download_blob()
         file = open("{}{}".format(downloads_path, blob), "wb")
         file.write(blob_data.readall())
         status = "Successfully Downloaded Blob {}".format(blob)
+        addLog("download", blob)
     except ResourceNotFoundError:
-        status = "File does not exist"
+        status = "Blob does not exist"
     return {"status": status}
 
 #  ----- Get List of Blobs in Storage -----
 @app.get("/list")
-async def list_files():
+async def listBlobs():
     list_blobs = []
     container_client = blob_service_client.get_container_client(container)
     blobs = container_client.list_blobs()
@@ -91,4 +94,5 @@ async def list_files():
         list_blobs.append({"Name": blob['name'], "Size": blob['size']})
     if len(list_blobs) == 0:
              list_blobs.append({"status": "No Blobs in Storage"})
+    addLog("list", "")
     return list_blobs
